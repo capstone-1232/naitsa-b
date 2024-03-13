7f575c92debeac2db2181204fabbff2c7ec05b9a
aaron <aaron.freeman41@gmail.com>
2024-03-13 14:12:58 -0600

    Created a divi child theme

---
+++ divi-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+function dt_enqueue_styles() {
+    $parenthandle = 'divi-style'; 
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(), // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') 
+    );
+}
+add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
