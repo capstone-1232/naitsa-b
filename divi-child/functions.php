f2767ca2c5f0f53b28301a5f294e45219d0c05e0
aaron <aaron.freeman41@gmail.com>
2024-04-03 15:39:27 -0600

    Dietary options layout

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -101,7 +101,7 @@ function display_menu_items()
                 ?>
     <div class="menu-item-container">
         <div class="menu-flex-container">
-            <div class="flex">
+            <div class="flex-row">
                 <h3><?php echo get_the_title(); ?></h3> <!-- title -->
                 <?php
                             
