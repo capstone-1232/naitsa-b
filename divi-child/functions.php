f4b4ee92e8b4bdaa7b27d996b2ff76518f1a0fc5
aaron <aaron.freeman41@gmail.com>
2024-04-05 18:31:42 -0600

    Minor functions changes

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -250,12 +250,12 @@ function display_menu_items()
                             <p><?php echo $menu_item_description = get_field('menu_item_description'); ?></p> <!-- description -->
                         </div>
                         <div class="menu-photo-container">
-                            <?php if ($menu_item_photo) : ?>
-                                <img src="<?php echo $menu_item_photo['url']; ?>" alt="<?php echo $menu_item_photo['alt']; ?>" class="menu-item-photo" width="100" height="auto">
-                                <?php else : ?>
-                                <?php $img_placeholder = wp_get_attachment_url(819); ?>
-                                <img src="<?php $img_placeholder ?>" alt="Placeholder" class="menu-item-photo" width="100" height="auto">    
-                            <?php endif; ?>
+                        <?php if ($menu_item_photo) : ?>
+                            <img src="<?php echo $menu_item_photo['url']; ?>" alt="<?php echo $menu_item_photo['alt']; ?>" class="menu-item-photo" width="100" height="auto">
+                        <?php else : ?>
+                            <?php $img_placeholder = wp_get_attachment_url(819); ?>
+                            <img src="<?php echo esc_url($img_placeholder); ?>" alt="Placeholder" class="menu-item-photo" width="100" height="auto">    
+                        <?php endif; ?>
                         </div>
                     </div>
                     <?php
